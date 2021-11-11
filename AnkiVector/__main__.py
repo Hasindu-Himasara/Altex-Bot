23a88b0a72e8ed8bafa966339b7f1e437350a343
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-11-11 15:22:44 +0530

    Update __main__.py

--- AnkiVector/__main__.py
+++ AnkiVector/__main__.py
@@ -85,7 +85,7 @@ buttons = [
     ],
 ]
 
-ANKIVECTOR_IMG = "https://telegra.ph/file/e38a91ff16aef15b31943.jpg"
+ANKIVECTOR_IMG = "https://telegra.ph/file/de4d282a662baf1517f64.jpg"
 
 HELP_STRINGS = f"""
 *Main Commands :* [🤖](https://telegra.ph/file/58bf0f76af528c2e2500f.jpg)
