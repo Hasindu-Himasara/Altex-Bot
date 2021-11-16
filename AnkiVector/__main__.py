6d75569ccb92733bf1dd2ab1e323d3e3eb01a1f7
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-11-16 21:13:11 +0530

    Update __main__.py

--- AnkiVector/__main__.py
+++ AnkiVector/__main__.py
@@ -227,7 +227,7 @@ def start(update: Update, context: CallbackContext):
             )
     else:
         update.effective_message.reply_text(
-            "I'm awake already!\n<b>Haven't slept since:</b> <code>{}</code>".format(
+            "🙋‍♂️ I'm awake already!\n<b>Haven't slept since 😴:</b> <code>{}</code>".format(
                 uptime
             ),
             parse_mode=ParseMode.HTML,
