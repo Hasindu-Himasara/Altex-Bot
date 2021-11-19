fcef4e814d12864163fba3710e57c7a41a9f8c65
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-11-19 16:14:49 +0530

    Update covid.py

--- AnkiVector/modules/covid.py
+++ AnkiVector/modules/covid.py
@@ -12,11 +12,11 @@ def covid(update: Update, context: CallbackContext):
     text = message.text.split(" ", 1)
     if len(text) == 1:
         r = requests.get("https://corona.lmao.ninja/v2/all").json()
-        reply_text = f"**Covid information search successfully ✔**\n\nGlobal Totals 🙋‍♂️\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Covid information search successfully ✅**\n◇───────────────◇\n\nGlobal Totals 🙋‍♂️\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     else:
         variabla = text[1]
         r = requests.get(f"https://corona.lmao.ninja/v2/countries/{variabla}").json()
-        reply_text = f"**Covid information search successful ✔**\\n☘ Cases for {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
+        reply_text = f"**Covid information search successful ✔** ✔──────────────✔ {r['country']} 🦠**\nCases: {r['cases']:,}\nCases Today: {r['todayCases']:,}\nDeaths: {r['deaths']:,}\nDeaths Today: {r['todayDeaths']:,}\nRecovered: {r['recovered']:,}\nActive: {r['active']:,}\nCritical: {r['critical']:,}\nCases/Mil: {r['casesPerOneMillion']}\nDeaths/Mil: {r['deathsPerOneMillion']}"
     message.reply_text(reply_text, parse_mode=ParseMode.MARKDOWN)
 
 
