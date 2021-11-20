0560e1d252ed4fad7ba178c7b8b566ab9bfc21d3
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-11-20 10:01:04 +0530

    Update zipper.py

--- AnkiVector/modules/zipper.py
+++ AnkiVector/modules/zipper.py
@@ -43,7 +43,7 @@ async def _(event):
     if event.is_group:
         if not (await is_register_admin(event.input_chat, event.message.sender_id)):
             await event.reply(
-                "Hai.. You are not admin.. You can't use this command.. But you can use in my pm"
+                "Hi.. You are not admin.. You can't use this command.. But you can use in my pm 😜"
             )
             return
 
@@ -125,7 +125,7 @@ async def _(event):
     if event.is_group:
         if not (await is_register_admin(event.input_chat, event.message.sender_id)):
             await event.reply(
-                " Hai.. You are not admin.. You can't use this command.. But you can use in my pm🙈"
+                " Hi.. You are not admin.. You can't use this command.. But you can use in my pm 👀"
             )
             return
 
@@ -149,7 +149,7 @@ async def _(event):
         with zipfile.ZipFile(downloaded_file_name, "r") as zip_ref:
             zip_ref.extractall(extracted)
         filename = sorted(get_lst_of_files(extracted, []))
-        await event.reply("Unzipping now")
+        await event.reply("successfully Unziped ✅\n\n◈──────────────────◈\n\n◈──────────────────◈")
         for single_file in filename:
             if os.path.exists(single_file):
                 caption_rts = os.path.basename(single_file)
