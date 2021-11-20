2eba159090fb486599c6f0648703fc648e1748b4
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-11-20 09:39:46 +0530

    Update __main__.py

--- AnkiVector/__main__.py
+++ AnkiVector/__main__.py
@@ -803,7 +803,7 @@ def main():
 
     if SUPPORT_CHAT is not None and isinstance(SUPPORT_CHAT, str):
         try:
-            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I am now online! ✅")
+            dispatcher.bot.sendMessage(f"@{SUPPORT_CHAT}", "I am now online! ✅ Bot Updated By Altex Team")
         except Unauthorized:
             LOGGER.warning(
                 "Bot isnt able to send message to support_chat, go and check!"
