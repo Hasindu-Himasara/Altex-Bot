57c52c07a34ff0dff45ad1c7c525e57f003a4342
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-12-07 21:31:44 +0530

    Update ssh.py

--- AnkiVector/modules/ssh.py
+++ AnkiVector/modules/ssh.py
@@ -23,3 +23,43 @@ def speedtestxyz(update: Update, context: CallbackContext):
     update.effective_message.reply_text(
         "Select SpeedTest Mode", reply_markup=InlineKeyboardMarkup(buttons)
     )
+
+    
+    @run_async
+def speedtestxyz_callback(update: Update, context: CallbackContext):
+    query = update.callback_query
+
+    if query.from_user.id in DEV_USERS:
+        msg = update.effective_message.edit_text("🚀 𝙍𝙪𝙣𝙣𝙞𝙣𝙜 𝙖 𝙨𝙥𝙚𝙚𝙙𝙩𝙚𝙨𝙩....")
+        speed = speedtest.Speedtest()
+        speed.get_best_server()
+        speed.download()
+        speed.upload()
+        replymsg = "SpeedTest Results:"
+
+        if query.data == "speedtest_image":
+            speedtest_image = speed.results.share()
+            update.effective_message.reply_photo(
+                photo=speedtest_image, caption=replymsg
+            )
+            msg.delete()
+
+        elif query.data == "speedtest_text":
+            result = speed.results.dict()
+            replymsg += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
+            update.effective_message.edit_text(replymsg, parse_mode=ParseMode.MARKDOWN)
+    else:
+        query.answer("You are required to join Heroes Association to use this command.")
+
+
+SPEED_TEST_HANDLER = DisableAbleCommandHandler("speedtest", speedtestxyz)
+SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
+    speedtestxyz_callback, pattern="speedtest_.*"
+)
+
+dispatcher.add_handler(SPEED_TEST_HANDLER)
+dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
+
+__mod_name__ = "SpeedTest"
+__command_list__ = ["speedtest"]
+__handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
