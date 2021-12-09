b97845219505880dfb0f8e8e8a1831788db2f54a
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2021-12-09 08:42:22 +0530

    Update speed_test.py

--- AnkiVector/modules/speed_test.py
+++ AnkiVector/modules/speed_test.py
@@ -2,7 +2,7 @@ import speedtest
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
-from AnkiVector import dispatcher
+from AnkiVector import DEV_USERS, dispatcher
 from AnkiVector.modules.disable import DisableAbleCommandHandler
 from AnkiVector.modules.helper_funcs.chat_status import dev_plus
 
@@ -16,12 +16,12 @@ def convert(speed):
 def speedtestxyz(update: Update, context: CallbackContext):
     buttons = [
         [
-            InlineKeyboardButton("🔥 SSH SSL 🔥", callback_data="sshacc_image"),
-            InlineKeyboardButton("🍀 SSH SSL 🍀", callback_data="sshacc_text"),
+            InlineKeyboardButton("Image", callback_data="speedtest_image"),
+            InlineKeyboardButton("Text", callback_data="speedtest_text"),
         ]
     ]
     update.effective_message.reply_text(
-        "\n🏖 23 Locations\n🔥 Unlimited Bandwith\n🚀 Fastest Servers\n🌺 100% Free\n☘@AltexSL_BOT", reply_markup=InlineKeyboardMarkup(buttons)
+        "Select SpeedTest Mode", reply_markup=InlineKeyboardMarkup(buttons)
     )
 
 
@@ -29,37 +29,37 @@ def speedtestxyz(update: Update, context: CallbackContext):
 def speedtestxyz_callback(update: Update, context: CallbackContext):
     query = update.callback_query
 
-  
-        msg = update.effective_message.edit_text("📡 Creating Your SSH Account. . . . . . .")
+    if query.from_user.id in DEV_USERS:
+        msg = update.effective_message.edit_text("Running a speedtest....")
         speed = speedtest.Speedtest()
         speed.get_best_server()
         speed.download()
         speed.upload()
-        replymsg = "☘️ Account Created Successfully ✅\n\n🇸🇬 Singapore 🇸🇬\nᗚ  Host IP : 134.209.105.165\nᗚ  Host Name : sg.ssl.sshmonth.com\nᗚ  Port : 443 or 444\nᗚ  Username : sshmonth.com-altex430\nᗚ  Password :  sshstore\nᗚ  Expiration  : 08-January-2022\nᗚ  Max Login  : Unlimited ♾\nᗚ  Torrents  : Torrents Not Allowed ❌:"
+        replymsg = "SpeedTest Results:"
 
-        if query.data == "sshacc_image":
+        if query.data == "speedtest_image":
             speedtest_image = speed.results.share()
             update.effective_message.reply_photo(
                 photo=speedtest_image, caption=replymsg
             )
- 
+            msg.delete()
 
-        elif query.data == "sshacc_text":
+        elif query.data == "speedtest_text":
             result = speed.results.dict()
-            replymsg = "☘️ Account Created Successfully ✅\n\n🇸🇬 Singapore 🇸🇬\nᗚ  Host IP : 134.209.105.165\nᗚ  Host Name : sg.ssl.sshmonth.com\nᗚ  Port : 443 or 444\nᗚ  Username : sshmonth.com-altex430\nᗚ  Password :  sshstore\nᗚ  Expiration  : 08-January-2022\nᗚ  Max Login  : Unlimited ♾\nᗚ  Torrents  : Torrents Not Allowed ❌"
+            replymsg += f"\nDownload: `{convert(result['download'])}Mb/s`\nUpload: `{convert(result['upload'])}Mb/s`\nPing: `{result['ping']}`"
             update.effective_message.edit_text(replymsg, parse_mode=ParseMode.MARKDOWN)
     else:
         query.answer("You are required to join Heroes Association to use this command.")
 
 
-SPEED_TEST_HANDLER = DisableAbleCommandHandler("sshacc", speedtestxyz)
+SPEED_TEST_HANDLER = DisableAbleCommandHandler("speedtest", speedtestxyz)
 SPEED_TEST_CALLBACKHANDLER = CallbackQueryHandler(
-    speedtestxyz_callback, pattern="sshacc_.*"
+    speedtestxyz_callback, pattern="speedtest_.*"
 )
 
 dispatcher.add_handler(SPEED_TEST_HANDLER)
 dispatcher.add_handler(SPEED_TEST_CALLBACKHANDLER)
 
-__mod_name__ = "☘ SSH ☘"
-__command_list__ = ["sshacc"]
+__mod_name__ = "SpeedTest"
+__command_list__ = ["speedtest"]
 __handlers__ = [SPEED_TEST_HANDLER, SPEED_TEST_CALLBACKHANDLER]
