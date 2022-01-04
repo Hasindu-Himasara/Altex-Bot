5e98003d19babf10f3737e4f1a83c73de08974b8
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2022-01-04 14:27:57 +0530

    Update __nightmode.py

--- AnkiVector/modules/__nightmode.py
+++ AnkiVector/modules/__nightmode.py
@@ -1,12 +1,12 @@
 import os
 
-from DewmiBot.modules.sql.night_mode_sql import add_nightmode, rmnightmode, get_all_chat_id, is_nightmode_indb
+from AnkiVector .modules.sql.night_mode_sql import add_nightmode, rmnightmode, get_all_chat_id, is_nightmode_indb
 from telethon.tl.types import ChatBannedRights
 from apscheduler.schedulers.asyncio import AsyncIOScheduler 
 from telethon import functions
-from DewmiBot.events import register
-from DewmiBot import OWNER_ID
-from DewmiBot import telethn as tbot
+from AnkiVector .events import register
+from AnkiVector import OWNER_ID
+from AnkiVector import telethn as tbot
 from telethon import *
 from telethon import Button, custom, events
 
