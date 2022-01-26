7c91168471f588f3e7e889e6910c9c71694c9a34
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2022-01-26 12:55:26 +0530

    Update admin.py

--- AnkiVector/modules/admin.py
+++ AnkiVector/modules/admin.py
@@ -501,7 +501,7 @@ dispatcher.add_handler(DEMOTE_HANDLER)
 dispatcher.add_handler(SET_TITLE_HANDLER)
 dispatcher.add_handler(ADMIN_REFRESH_HANDLER)
 
-__mod_name__ = "🇦 🇩 🇲 🇮 🇳"
+__mod_name__ = "🇦🇩🇲🇮🇳 🎉"
 __command_list__ = [
     "adminlist",
     "admins",
