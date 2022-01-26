bcc4bd81ba37d4fb2501c9b4f93a55f700731a80
Hasindu Himasara <90503467+Hasindu-Himasara@users.noreply.github.com>
2022-01-26 13:14:36 +0530

    Update yt.py

--- AnkiVector/modules/yt.py
+++ AnkiVector/modules/yt.py
@@ -2,4 +2,4 @@ __help__ = """
  ❍ /video <text>: perform a youtube search
  ❍ /song <link> or /video <link>: Downlods a video or audio from a youtube video to the bots local server
 """
-__mod_name__ = "ʏᴏᴜᴛᴜʙᴇ📺"
+__mod_name__ = "YouTube📺"
